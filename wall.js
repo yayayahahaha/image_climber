27601177eb3a1efeb955eeb7a2cde0aa5025fab1
flyc <yayayahahahaooii@gmail.com>
2018-09-21 00:13:24 +0800

    use single quote rather than double quote

--- wall.js
+++ wall.js
@@ -1,7 +1,7 @@
-var request = require("request");
-var fs = require("fs");
-var cheerio = require("cheerio");
-var axios = require("axios");
+var request = require('request');
+var fs = require('fs');
+var cheerio = require('cheerio');
+var axios = require('axios');
 
 var key_words = process.argv[2] ? process.argv[2] : null,
     directory = process.argv[3] ? process.argv[3] : key_words,
@@ -10,29 +10,29 @@ var key_words = process.argv[2] ? process.argv[2] : null,
     totalImagesNumber = 0,
     imagesInformations = [],
     countloaded = 0,
-    log = "";
+    log = '';
 
 /* create directory */
 if (!!!key_words) {
-    console.log("please try \"$npm start {{key_words}} {{directory}}\" again!");
+    console.log('please try \'$npm start {{key_words}} {{directory}}\' again!');
 } else {
     /* start */
 
     /* get total page number */
     var r = request.get(
-        "https://wall.alphacoders.com/search.php?search=" + encodeURI(key_words) + "&page=" + 9999999999,
+        'https://wall.alphacoders.com/search.php?search=' + encodeURI(key_words) + '&page=' + 9999999999,
         function(err, res, body) {
             totalPagesNumber = res.request.uri.href;
-            totalPagesNumber = totalPagesNumber.split("&page=")[1];
+            totalPagesNumber = totalPagesNumber.split('&page=')[1];
             totalPagesNumber = parseInt(totalPagesNumber);
             if (totalPagesNumber === 9999999999) {
-                console.log("Sorry, we have no results for your search! please try another key_words");
+                console.log('Sorry, we have no results for your search! please try another key_words');
                 return;
             }
-            insertLog("key words: " + key_words);
-            insertLog("directory: " + directory);
+            insertLog('key words: ' + key_words);
+            insertLog('directory: ' + directory);
 
-            insertLog("total page number: " + totalPagesNumber);
+            insertLog('total page number: ' + totalPagesNumber);
 
             getPageNumber(startPage);
         });
@@ -42,11 +42,11 @@ if (!!!key_words) {
 function getPageNumber(nowPage) {
     axios({
             method: 'get',
-            url: "https://wall.alphacoders.com/search.php?search=" + encodeURI(key_words) + "&page=" + nowPage,
+            url: 'https://wall.alphacoders.com/search.php?search=' + encodeURI(key_words) + '&page=' + nowPage,
         })
         .then(function(res) {
             var $ = cheerio.load(res.data),
-                list = $(".thumb-container-big .boxgrid a img"),
+                list = $('.thumb-container-big .boxgrid a img'),
                 src = null;
 
             imagesInformations = getImageInfo(nowPage, list, $, imagesInformations);
@@ -59,7 +59,7 @@ function getPageNumber(nowPage) {
                     fs.mkdirSync(key_words);
                 }
 
-                console.log("\ndownload started!\n");
+                console.log('\ndownload started!\n');
                 totalImagesNumber = imagesInformations.length;
                 for (var i = 0; i < imagesInformations.length; i++) {
                     obj = imagesInformations[i];
@@ -82,40 +82,40 @@ function insertLog(input, hide) {
         console.log(input);
     }
     log += new Date();
-    log += "\n";
+    log += '\n';
     log += input;
-    log += "\n";
-    log += "\n";
+    log += '\n';
+    log += '\n';
 }
 
 function endingPoint() {
-    var log_name = key_words + " " + (new Date()).toString().replace(/\./g, "-").replace(/\:/g, "-") + ".txt";
+    var log_name = key_words + ' ' + (new Date()).toString().replace(/\./g, '-').replace(/\:/g, '-') + '.txt';
 
-    insertLog("Downloaded finished", true);
-    console.log("\n=========================================");
-    console.log("all iamges are downloaded!");
-    console.log("check out \"" + log_name + "\" for more information.");
-    console.log("=========================================");
+    insertLog('Downloaded finished', true);
+    console.log('\n=========================================');
+    console.log('all iamges are downloaded!');
+    console.log('check out \'' + log_name + '\' for more information.');
+    console.log('=========================================');
     fs.writeFileSync(log_name, log);
 }
 
 function download(url, dir, filename, total) {
-    insertLog("start download " + filename, true);
+    insertLog('start download ' + filename, true);
     request(url, function(er, res, body) {
         if (er) {
-            insertLog("retry: " + filename);
+            insertLog('retry: ' + filename);
             download(url, dir, filename, total);
             return;
         }
         countloaded++;
-        console.log(countloaded + " / " + total + " || " + Math.round(countloaded * 1000 / total) / 10 + "%");
+        console.log(countloaded + ' / ' + total + ' || ' + Math.round(countloaded * 1000 / total) / 10 + '%');
 
         /* check all files downloaded or not */
         if (countloaded === total) {
             endingPoint();
         }
 
-    }).pipe(fs.createWriteStream(dir + "/" + filename));
+    }).pipe(fs.createWriteStream(dir + '/' + filename));
 }
 
 function combineRouteAndID(input) {
@@ -124,7 +124,7 @@ function combineRouteAndID(input) {
         obj = input[i];
         tmp.push({
             name: obj.id,
-            src: "https://" + obj.header + ".alphacoders.com/" + obj.route + "/" + obj.id
+            src: 'https://' + obj.header + '.alphacoders.com/' + obj.route + '/' + obj.id
         });
 
         obj = null;
@@ -136,19 +136,19 @@ function getImageInfo(nowPage, list, $, inputArray, checker) {
     var imagesInformations = inputArray.slice();
 
     for (var i = 0; i < list.length; i++) {
-        src = $(list[i]).attr("src").split(".com/")[1];
-        header = $(list[i]).attr("src").split(".com/")[0];
+        src = $(list[i]).attr('src').split('.com/')[1];
+        header = $(list[i]).attr('src').split('.com/')[0];
         imagesInformations.push({
             page: nowPage,
-            route: src.split("/")[0],
-            id: src.split("/")[1].split("-")[2],
-            header: header.split(".alphacoders")[0].split("//")[1]
+            route: src.split('/')[0],
+            id: src.split('/')[1].split('-')[2],
+            header: header.split('.alphacoders')[0].split('//')[1]
         });
 
         src = null;
     }
     if (!checker) {
-        insertLog("get page " + nowPage + " data success");
+        insertLog('get page ' + nowPage + ' data success');
     }
     return imagesInformations;
 }
