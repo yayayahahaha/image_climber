8d11d5a6e62d52c8bd1a67c9c60effd96819bd64
flyc <yayayahahahaooii@gmail.com>
2019-03-14 01:34:50 +0800

    downlaod image success, Next: create folder

--- wall.mjs
+++ wall.mjs
@@ -136,5 +136,70 @@ async function getAllImageUrl(allImagesId) {
 }
 
 async function startDownLoad(allImagesSrc) {
-    console.log(allImagesSrc.length);
+    var taskArray = [],
+        task_search = null;
+
+    allImagesSrc.forEach(function(src) {
+        taskArray.push(_createReturnFunctoin(src));
+    });
+
+    task_search = new TaskSystem(taskArray.splice(0, 10), 5);
+    var response = await task_search.doPromise();
+
+    fs.writeFileSync('final-object.json', JSON.stringify(taskArray, null, 2));
+
+    function _createReturnFunctoin(src) {
+        var splitResult = src.split('/'),
+            url = src,
+            filePath = splitResult[splitResult.length - 1];
+        return function() {
+            return download(url, filePath);
+        };
+    }
+}
+
+// copy from pixiv project
+function download(url, filePath, headers = {}, {
+    callback = Function.prototype,
+    callbackParameter = undefined
+} = {}) {
+    return new Promise(async (resolve, reject) => {
+        // 濾掉尾巴的斜線
+        if (/\/$/.test(filePath)) {
+            filePath = filePath.slice(0, filePath.length - 1);
+        }
+        // 濾掉開頭的./
+        if (/^\.\//.test(filePath)) {
+            filePath = filePath.slice(2, filePath.length);
+        }
+
+        // 如果資料夾不存在會自動創建的系統
+        var paths = filePath.split('/'),
+            createdDirectory = [];
+        for (var i = 0; i < paths.length - 1; i++) {
+            createdDirectory.push(paths[i]);
+            var checkedDirectory = createdDirectory.join('/');
+            !fs.existsSync(checkedDirectory) && fs.mkdirSync(checkedDirectory);
+        }
+
+        var file = fs.createWriteStream(filePath);
+        axios({
+            method: 'get',
+            url: url,
+            responseType: 'stream',
+            headers: headers
+        }).then(({
+            data
+        }) => {
+            callback(true, callbackParameter);
+            data.pipe(file);
+            file.on('finish', () => {
+                resolve(true);
+            });
+        }).catch((error) => {
+            console.log(error);
+            callback(false, callbackParameter);
+            reject([null, error]);
+        });
+    });
 }
