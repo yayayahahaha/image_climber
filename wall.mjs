05086ac6251579ca718ec0c8e4e562ac8bd57fc0
flyc <yayayahahahaooii@gmail.com>
2019-03-14 00:50:32 +0800

    separate baseUrl with its domain and router

--- wall.mjs
+++ wall.mjs
@@ -8,7 +8,7 @@ import {
 
 var keyword = process.argv[2] ? process.argv[2] : false,
     directory = process.argv[3] ? process.argv[3] : String(keyword),
-    baseUrl = 'https://wall.alphacoders.com/search.php',
+    baseUrl = 'https://wall.alphacoders.com',
     startPage = 1,
     imagesInformations = [],
     countloaded = 0,
@@ -21,7 +21,7 @@ init();
 async function getTotalImageNumber() {
     var totalImagesNumber = await axios({
         method: 'get',
-        url: baseUrl,
+        url: baseUrl + '/search.php',
         data: {
             search: keyword,
             page: 1
@@ -65,7 +65,7 @@ async function getAllImagesId(page_number) {
     fs.writeFileSync('result.json', JSON.stringify(response));
 
     function _createReturnFunction(page) {
-        var url = baseUrl + '?search=' + keyword + '&page=' + page
+        var url = baseUrl + '/search.php?search=' + keyword + '&page=' + page
         return function() {
             return axios({
                 method: 'get',
@@ -90,7 +90,7 @@ async function getAllImagesId(page_number) {
 function getPageNumber(nowPage) {
     axios({
             method: 'get',
-            url: baseUrl + '?search=' + encodeURI(keyword) + '&page=' + nowPage,
+            url: baseUrl + '/search.php?search=' + encodeURI(keyword) + '&page=' + nowPage,
         })
         .then(function(res) {
             var $ = cheerio.load(res.data),
